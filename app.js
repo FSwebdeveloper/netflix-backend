1d3e70eeef24d566f48db8e589e14a80d689e4df
Achinta Haldar <107491271+FSwebdeveloper@users.noreply.github.com>
2024-02-07 21:08:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ res.json(data);
 
 
 let port = process.env.PORT;
-if (port == null || port == "https://netflix-backend-dgl1.onrender.com") {
+if (port == null || port == "") {
   port = 8080;
 }
 app.listen(port, () => {
